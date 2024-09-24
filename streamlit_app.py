6d3d9a0df002e26ff5356115a12d9cde6177e3d2
Andrianihary RAZAFINDRAMISA <andrianihary-tsiory.razafindramisa@wavestone.com>
2024-09-24 15:29:26 +0200

    Test 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,11 @@ question = st.text_input("Enter your question:")
 context = st.text_area("Provide the context for your question:")
 
 # Button to send the question and context
-print(1)
+st.write(1)
 if st.button("Get Answer"):
-    print(2)
+    st.write(2)
     if question and context:
-        print(3)
+        st.write(3)
         # Prepare the payload (data) to send to the Lambda function
         payload = {
             "question": question,
@@ -32,36 +32,36 @@ if st.button("Get Answer"):
         arn_url = "arn:aws:lambda:eu-west-1:640167380126:function:CallSageMakerLLM"
 
         try:
-            print(4)
+            st.write(4)
             # Invoke Lambda function
             response = lambda_client.invoke(
                 FunctionName=arn_url,
                 InvocationType='RequestResponse',
                 Payload=json.dumps(payload)
             )
-            print(response)
+            st.write(response)
 
             # Read and parse response
             response_payload = json.loads(response['Payload'].read())
             
             # Check for errors in the response
             if 'FunctionError' in response:
-                print(5)
+                st.write(5)
                 st.error(f"Lambda function error: {response['FunctionError']}")
 
             # Check if the request was successful
             if response_payload.get('statusCode') == 200:
-                print(6)
+                st.write(6)
                 # Parse and display the result
                 result = json.loads(response_payload['body'])
                 st.write(f"Question: {result['Question']}")
                 st.write(f"Answer: {result['Message']}")
             else:
-                print(7)
+                st.write(7)
                 st.error(f"Error: Could not get a valid response, status code: {response_payload.get('statusCode')}")
         except Exception as e:
-            print(8)
+            st.write(8)
             st.error(f"Error invoking Lambda: {e}")
     else:
-        print(9)
+        st.write(9)
         st.error("Please enter both a question and context.")
